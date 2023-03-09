9799fd92f3373e9c0a4a0f97bb5a5ae024be12fc
kwameaj67 <kwameagyenimboateng64@gmail.com>
2023-03-09 12:38:02 +0000

    map vc & delegates

--- rental_property/Controllers/Map/MapVC+Delegates.swift
+++ rental_property/Controllers/Map/MapVC+Delegates.swift
@@ -6,5 +6,85 @@
 //
 
 import UIKit
+import GoogleMaps
 
 
+
+
+// MARK: CLLocationManagerDelegate -
+extension MapVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        coordinate = location.coordinate
+        mapView.camera = .init(latitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 15.0, bearing: 0, viewingAngle: 10)
+        marker.position = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+    }
+}
+
+// MARK: GMSMapViewDelegate-
+extension MapVC : GMSMapViewDelegate {
+   
+    func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
+        UIView.animate(withDuration: 0.5) {
+
+        } completion: { _ in
+            self.marker.tracksViewChanges = false
+        }
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        let coordinate = marker.position
+        print("You tapped at \(coordinate.latitude), \(coordinate.longitude)")
+
+        return true
+    }
+   
+}
+
+// MARK: UIToolbar -
+extension MapVC {
+    func createToolBar() -> UIToolbar{
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let space1 = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let space2 = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let labelButton = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        labelButton.setTitle("Done", for: .normal)
+        labelButton.setTitleColor(.black, for: .normal)
+        labelButton.titleLabel?.font = custom(name: .medium, size: 18, style: .headline)
+        labelButton.addTarget(self, action: #selector(onDone), for: .primaryActionTriggered)
+        let doneBarItem = UIBarButtonItem(customView: labelButton)
+        doneBarItem.tintColor = UIColor.black
+        toolbar.setItems([space1,space2,doneBarItem], animated: true)
+        return toolbar
+    }
+    
+    @objc func onDone(){
+        let textField = searchView.searchTextField
+        if textField.isFirstResponder{
+            textField.resignFirstResponder()
+        }
+    }
+}
+
+// MARK: DidTapGPSDelegate -
+extension MapVC: DidTapGPSDelegate {
+    func didTapGPS() {
+        print("DEBUG: hanlding didTapGPS...")
+        let position = GMSCameraPosition(latitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 15.0, bearing: 0, viewingAngle: 10)
+        marker.position = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        mapView.animate(to: position)
+    }
+}
+
+
+// MARK: DidTapFilterButtonDelegate -
+extension MapVC: DidTapFilterButtonDelegate {
+    func didTapFilterButton() {
+        print("DEBUG: hanlding didTapFilterButton...")
+        let vc = FilterVC()
+//        vc.modalPresentationStyle = .overCurrentContext
+//        vc.modalTransitionStyle = .crossDissolve
+        present(vc, animated: true)
+    }
+}
--- rental_property/Controllers/Map/MapVC.swift
+++ rental_property/Controllers/Map/MapVC.swift
@@ -19,9 +19,9 @@ class MapVC: UIViewController {
     let data = Marker.data
     
     override var preferredStatusBarStyle: UIStatusBarStyle{
-        .darkContent
+        return .darkContent
     }
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -31,12 +31,12 @@ class MapVC: UIViewController {
         setupMapView()
         setupViews()
         setupContraints()
-        
     }
 
     // MARK: Properties -
     lazy var searchView: SearchView = {
         let v = SearchView(frame: .zero)
+        v.delegate = self
         v.searchTextField.inputAccessoryView = createToolBar()
         return v
     }()
@@ -89,22 +89,19 @@ class MapVC: UIViewController {
         // marker
         let image = UIImage(named: "map-pin")?.withRenderingMode(.alwaysOriginal)
         marker.icon = sizeImage(image!, scaledToSize: .init(width: 40, height: 40))
-        marker.tracksInfoWindowChanges = true
+        marker.tracksViewChanges = true
         marker.map = mapView
         
         //show markers
         DispatchQueue.main.async(execute: {
-//            var dict = [String:String]()
             self.data.forEach { item in
                 let marker = GMSMarker(position: CLLocationCoordinate2D(latitude: item.lat, longitude: item.lng))
                 marker.title = item.name
                 marker.tracksViewChanges = false
-                marker.iconView = self.infoView
+//                marker.iconView = self.infoView
+                marker.icon = sizeImage((UIImage(named: "circle")?.withRenderingMode(.alwaysOriginal))!, scaledToSize: .init(width: 50, height: 50))
                 marker.isFlat = true
-                self.infoView.markerDetail = item
-                
-//                dict["name"] = item.name
-//                marker.userData = dict
+//                self.infoView.markerDetail = item
 //                print("DEBUG:Labels \(self.infoViewx.markerDetail)")
                 marker.map = self.mapView
             }
@@ -140,80 +137,5 @@ class MapVC: UIViewController {
             infoView.widthAnchor.constraint(equalToConstant: 120),
         ])
     }
-    func animateMarker(_ animateTo: UIView){
-        UIView.animate(withDuration: 1.5, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.0, options: [.repeat,.autoreverse,.curveLinear,.allowUserInteraction,], animations: {
-                animateTo.transform = CGAffineTransform(scaleX: 0.96, y: 0.96)
-            }, completion: nil)
-    }
 }
 
-// MARK: CLLocationManagerDelegate -
-extension MapVC: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.first else { return }
-        coordinate = location.coordinate
-        mapView.camera = .init(latitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 15.0, bearing: 0, viewingAngle: 10)
-        marker.position = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
-    }
-}
-
-// MARK: GMSMapViewDelegate-
-extension MapVC : GMSMapViewDelegate {
-    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
-        let view = InfoView(frame: .zero)
-        data.map { item in
-            view.markerDetail = item
-        }
-        return view
-    }
-    
-    func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
-        UIView.animate(withDuration: 0.5) {
-            
-        } completion: { _ in
-            self.marker.tracksViewChanges = false
-        }
-    }
-    
-    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
-        print("You tapped at \(coordinate.latitude), \(coordinate.longitude)")
-    }
-    
-    
-}
-
-// MARK: UIToolbar -
-extension MapVC {
-    func createToolBar() -> UIToolbar{
-        let toolbar = UIToolbar()
-        toolbar.sizeToFit()
-        let space1 = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let space2 = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let labelButton = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
-        labelButton.setTitle("Done", for: .normal)
-        labelButton.setTitleColor(.black, for: .normal)
-        labelButton.titleLabel?.font = custom(name: .medium, size: 18, style: .headline)
-        labelButton.addTarget(self, action: #selector(onDone), for: .primaryActionTriggered)
-        let doneBarItem = UIBarButtonItem(customView: labelButton)
-        doneBarItem.tintColor = UIColor.black
-        toolbar.setItems([space1,space2,doneBarItem], animated: true)
-        return toolbar
-    }
-    
-    @objc func onDone(){
-        let textField = searchView.searchTextField
-        if textField.isFirstResponder{
-            textField.resignFirstResponder()
-        }
-    }
-}
-
-// MARK: DidTapGPSDelegate - 
-extension MapVC: DidTapGPSDelegate {
-    func didTapGPS() {
-        print("DEBUG: hanlding didTapGPS...")
-        let position = GMSCameraPosition(latitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 15.0, bearing: 0, viewingAngle: 10)
-        marker.position = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
-        mapView.animate(to: position)
-    }
-}
