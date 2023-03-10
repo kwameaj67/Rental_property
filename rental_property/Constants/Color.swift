78a8f847506121eaa5a71e3b36e08481d36ce412
kwameaj67 <kwameagyenimboateng64@gmail.com>
2023-03-10 19:01:35 +0000

    change hex colors to ui colors

--- rental_property/Constants/Color.swift
+++ rental_property/Constants/Color.swift
@@ -5,11 +5,11 @@
 //  Created by Kwame Agyenim Boateng on 3/5/23.
 //
 
-import Foundation
+import UIKit
 
 
-enum Color: String {
-    case black = "#242424"
-    case grey = "#f9fafb"
-    case bg = "#ffffff"
+struct Color {
+    static let black = UIColor(red: 0.14, green: 0.14, blue: 0.14, alpha: 1.00)
+    static let grey = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.00)
+    static let bg = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)
 }
