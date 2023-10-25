3b882fddb0a3ae9d3dd193a0944acebdabaca60f
Kwame <kwameagyenimboateng64@gmail.com>
2023-10-25 18:09:03 +0000

    move tool bar code to search view

--- rental_property/App/UI/Views/Map/SearchView.swift
+++ rental_property/App/UI/Views/Map/SearchView.swift
@@ -31,6 +31,7 @@ class SearchView: UIView {
     lazy var searchBtn: UIButton = {
         let btn = UIButton()
         btn.setImage(UIImage(named: "search")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        btn.addTarget(self, action: #selector(handleSearchDidTap), for: .touchUpInside)
         btn.translatesAutoresizingMaskIntoConstraints = false
         return btn
     }()
@@ -42,13 +43,15 @@ class SearchView: UIView {
         tf.autocapitalizationType = .none
         tf.textColor = .black
         tf.font = custom(name: .medium, size: 17, style: .caption1)
+        tf.inputAccessoryView = createToolBar()
         tf.translatesAutoresizingMaskIntoConstraints = false
         return tf
     }()
+    
     lazy var filterBtn: UIButton = {
         let btn = UIButton()
         btn.setImage(UIImage(named: "setting")?.withRenderingMode(.alwaysOriginal), for: .normal)
-        btn.addTarget(self, action: #selector(handleFilterTap), for: .touchUpInside)
+        btn.addTarget(self, action: #selector(handleFilterDidTap), for: .touchUpInside)
         btn.translatesAutoresizingMaskIntoConstraints = false
         return btn
     }()
@@ -78,8 +81,37 @@ class SearchView: UIView {
         ])
     }
     
-    @objc func handleFilterTap(){
+    @objc func handleFilterDidTap(){
         generateHapticTouch()
         delegate?.didTapFilterButton()
+    }
+    
+    @objc func handleSearchDidTap(){
+        searchTextField.becomeFirstResponder()
+    }
+}
+
+// MARK: UIToolbar -
+extension SearchView {
+    func createToolBar() -> UIToolbar{
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let space1 = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let space2 = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let labelButton = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        labelButton.setTitle("Done", for: .normal)
+        labelButton.setTitleColor(.black, for: .normal)
+        labelButton.titleLabel?.font = custom(name: .medium, size: 18, style: .headline)
+        labelButton.addTarget(self, action: #selector(onDone), for: .primaryActionTriggered)
+        let doneBarItem = UIBarButtonItem(customView: labelButton)
+        doneBarItem.tintColor = UIColor.black
+        toolbar.setItems([space1,space2,doneBarItem], animated: true)
+        return toolbar
+    }
+    
+    @objc func onDone(){
+        if searchTextField.isFirstResponder{
+            searchTextField.resignFirstResponder()
+        }
     }
 }
